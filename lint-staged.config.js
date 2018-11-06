4b2ca5087755e4fcc9658230a34b8ec18904d134
Hyunmyung Park <stophobia@gmail.com>
2018-11-06 23:39:38 +0900

    init

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.js': ['eslint --fix', 'prettier --write', 'git add'],
-  '*.json': ['prettier --write', 'git add'],
-  '*.vue': ['eslint --fix', 'stylelint --fix', 'prettier --write', 'git add'],
-  '*.scss': ['stylelint --fix', 'prettier --write', 'git add'],
-  '*.md': ['markdownlint', 'prettier --write', 'git add'],
-  '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
-}
